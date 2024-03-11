7483c22e007687d62e8e3e3002500f2909ede9cf
Timur Bashirov <bashirov188@gmail.com>
2024-03-11 17:28:15 +0300

    done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,15 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+
+       int  product = 1;
+        while(number>0){
+
+            int digit = number%10;
+            product *= digit;
+            number/=10;
+
+        }
+        System.out.println(product);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,16 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        String string1 = String.valueOf(number);
+
+        StringBuilder str = new StringBuilder(string1);
+
+        String stringRev = str.reverse().toString();
+
+        int numerReversed = Integer.valueOf(stringRev);
+
+        System.out.println(numerReversed);
     }
+
+
 }
