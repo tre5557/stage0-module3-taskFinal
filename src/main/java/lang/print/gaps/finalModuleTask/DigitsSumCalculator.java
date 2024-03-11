c16af1d568c08782d5441df31df2f955e6c910ba
Timur Bashirov <bashirov188@gmail.com>
2024-03-11 17:38:19 +0300

    done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -3,7 +3,7 @@ package lang.print.gaps.finalModuleTask;
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
 
-       int  product = 1;
+       int  product = 0;
         while(number>0){
 
             int digit = number%10;
